1d4a554ed1ea2bdf6428474c7c065cb2bd443c90
Jonathan Camenisch <jonathan.camenisch@fortra.com>
2024-02-15 17:43:33 -0500

    Modify to return the canonical name (preserving previous behavior)

--- lib/spf/util.rb
+++ lib/spf/util.rb
@@ -45,7 +45,7 @@ module SPF
   end
 
   def self.hostname
-    return @hostname ||= Addrinfo.getaddrinfo(Socket.gethostname, 0).first.getnameinfo.first
+    return @hostname ||= Addrinfo.getaddrinfo(Socket.gethostname, nil, nil, nil, nil, Socket::AI_CANONNAME).first.canonname
   rescue SocketError
     return @hostname ||= Socket.gethostname
   end
